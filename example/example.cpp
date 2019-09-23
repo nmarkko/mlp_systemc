cf373980d1dec9afd2e07faad2663784e4bfd9de
nmarkko <50952339+nmarkko@users.noreply.github.com>
2019-09-23 02:13:15 +0200

    Update example.cpp

--- example/example.cpp
+++ example/example.cpp
@@ -11,11 +11,12 @@ using namespace std;
 
 int main() {
 	fstream input_data;
-	input_data.open("input_data.hpp", fstream::out);
+	input_data.open("input_data_@_home.hpp", fstream::out);
 	double A[X][Y];
 	double B[30];
+	double C[784];
 
-	ifstream f("weights_1.txt");	
+	ifstream f("weights_1.txt");
 	input_data << "weights_1[30][784] = {";
 
 	for (int i = 0; i < X; i++) {
@@ -24,18 +25,18 @@ int main() {
 			f >> A[i][j];
 			cout << A[i][j];
 			input_data << A[i][j];
-			if(j!=Y-1) input_data <<", ";	
-	
+			if(j!=Y-1) input_data <<", ";
+
 		}
 		input_data << "}";
 		if(i!=X-1) input_data <<", ";
 		cout << endl;
 	}
-	input_data << "}" << endl;
+	input_data << "};" << endl;
 	f.close();
 
 //------------------------------------------
-	f.open("weights_2.txt");	
+	f.open("weights_2.txt");
 	input_data << "weights_2[10][30] = {";
 
 	for (int i = 0; i < 10; i++) {
@@ -44,43 +45,55 @@ int main() {
 			f >> A[i][j];
 			cout << A[i][j];
 			input_data << A[i][j];
-			if(j!=30-1) input_data <<", ";	
-	
+			if(j!=30-1) input_data <<", ";
+
 		}
 		input_data << "}";
 		if(i!=10-1) input_data <<", ";
 		cout << endl;
 	}
-	input_data << "}" << endl;
+	input_data << "};" << endl;
 	f.close();
 //-------------------------------------
 
-	f.open("biases_1.txt");	
+	f.open("biases_1.txt");
 	input_data << "biases_1[30] = {";
 
 	for (int i = 0; i < 30; i++) {
 			f >> B[i];
 			cout << B[i];
 			input_data << B[i];
-			if(i!=30-1) input_data <<", ";	
+			if(i!=30-1) input_data <<", ";
 	}
-	input_data << "}" << endl;
+	input_data << "};" << endl;
 	f.close();
 //-------------------------------------
-	f.open("biases_2.txt");	
+	f.open("biases_2.txt");
 	input_data << "biases_2[10] = {";
 
 	for (int i = 0; i < 10; i++) {
 			f >> B[i];
 			cout << B[i];
 			input_data << B[i];
-			if(i!=10-1) input_data <<", ";	
+			if(i!=10-1) input_data <<", ";
 	}
-	input_data << "}" << endl;
+	input_data << "};" << endl;
+	f.close();
+	
+//---------------------------------
+	f.open("BROJ.txt");
+	input_data << "slika[784] = {";
+
+	for (int i = 0; i < 784; i++) {
+			f >> C[i];
+			cout << C[i];
+			input_data << C[i];
+			if(i!=10-1) input_data <<", ";
+	}
+	input_data << "};" << endl;
 	f.close();
 
 
-
 	input_data.close();
-	
+
 }
